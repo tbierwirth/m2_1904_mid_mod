988b7e921c37deb571cb946bea4264734ba68427
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-14 14:16:41 -0600

    Create migrations for books, authors and join table

---
+++ db/migrate/20190713145343_create_books.rb
@@ -0,0 +1,9 @@
+class CreateBooks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.integer :pages
+      t.integer :publication_year
+    end
+  end
+end
