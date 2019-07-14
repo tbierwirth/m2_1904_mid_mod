c893eea4d0e457133a28b6f1fd7f9c690a2f5d9f
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-14 14:20:49 -0600

    Change test for association

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Author, type: :model do
 
   describe "Relationships" do
     it {should have_many :author_books}
-    it {should have_many :books}
+    it {should have_many(:books).through(:author_books)}
   end
 
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Book, type: :model do
 
   describe "Relationships" do
     it {should have_many :author_books}
-    it {should have_many :authors}
+    it {should have_many(:authors).through(:author_books)}
   end
 
 end
